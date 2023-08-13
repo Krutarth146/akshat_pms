801ee312a112fc1cff05de1c1e7af734b5e7f714
Krutarth Patel <krutarthpatel146@gmail.com>
2023-08-13 16:05:10 +0530

    Templates

--- pms/main/urls.py
+++ pms/main/urls.py
@@ -3,4 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('login/',login,name='login'),
+    path('signup/',signup,name='signup'),
+    path('equipment/',equipment,name='equipment'),
+    path('contact/',contact,name='contact'),
 ]
--- pms/main/views.py
+++ pms/main/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, 'main/login.html')
+    
+def signup(request):
+    return render(request, 'main/signup.html')
+
+def equipment(request):
+    return render(request, 'main/equipment.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+
